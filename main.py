27c70a9865c5cdc220913f0dcf06ddf17cb17b4d
v1chi <130424809+v1chi@users.noreply.github.com>
2024-05-21 01:04:29 -0400

    Update main.py
    Menu Admin listoco

--- main.py
+++ main.py
@@ -14,22 +14,6 @@ def connection():
         cursor.close() # Cerramos el objetos cursor para (Cursor permite ejecutar los comandos sql)
         return None
 
-def select_query(query, data=[]):
-    try:
-        if connection() and query != '' and data == []:
-            cursor = connection().cursor()
-            cursor.execute(query)
-            result = cursor.fetchall()
-            return result
-        elif connection() and query != '' and data != []:
-            cursor = connection().cursor()
-            cursor.execute(query, tuple(data))
-            result = cursor.fetchall()
-            return result
-    except(Exception, Error) as error:
-        print("Error: %s" % error)
-        connection.rollback()
-
 #Función para crear la tabla de usuarios
 def create_users_table():
     try:
@@ -54,7 +38,8 @@ def create_users_table():
 # Función para crear la tabla de productos
 def create_products_table():
     try:
-        cursor = connection().cursor()
+        conn = connection()
+        cursor = conn.cursor()
         create_table_query = """
         CREATE TABLE IF NOT EXISTS productos (
             nombre text primary key,
@@ -64,7 +49,7 @@ def create_products_table():
         );
         """
         cursor.execute(create_table_query)
-        connection().commit()
+        conn.commit()
         print("Tabla de productos creada exitosamente.")
     except psycopg2.Error as e:
         print("Error al crear la tabla de productos:", e)
@@ -72,7 +57,8 @@ def create_products_table():
 # Función para crear la tabla de ventas
 def create_ventas_table():
     try:
-        cursor = connection().cursor()
+        conn = connection()
+        cursor = conn.cursor()
         create_table_query = """
         CREATE TABLE IF NOT EXISTS ventas (
             id int PRIMARY KEY,
@@ -82,7 +68,7 @@ def create_ventas_table():
         );
         """
         cursor.execute(create_table_query)
-        connection().commit()
+        conn.commit()
         print("Tabla de ventas creada exitosamente.")
     except psycopg2.Error as e:
         print("Error al crear la tabla de ventas:", e)
@@ -90,7 +76,8 @@ def create_ventas_table():
 # Función para crear la tabla de clientes
 def create_clientes_table():
     try:
-        cursor = connection().cursor()
+        conn = connection()
+        cursor = conn.cursor()
         create_table_query = """
         CREATE TABLE IF NOT EXISTS clientes (
             username text primary key references usuarios(username),
@@ -100,7 +87,7 @@ def create_clientes_table():
         );
         """
         cursor.execute(create_table_query)
-        connection().commit()
+        conn.commit()
         print("Tabla de clientes creada exitosamente.")
     except psycopg2.Error as e:
         print("Error al crear la tabla de clientes:", e)
@@ -122,9 +109,10 @@ def is_valid_password(password):
     return True
 
 #Ingresar un cliente en la base de datos
-def register_client(username, email, direccion, nombre, conn):
+def register_client(username, email, direccion, nombre):
     try:
         insert_query = "INSERT INTO clientes (username, nombre, email, direccion) VALUES (%s, %s, %s, %s)"
+        conn = connection()
         cursor = conn.cursor()
         cursor.execute(insert_query, (username, nombre, email, direccion))
         conn.commit()
@@ -136,7 +124,7 @@ def register_client(username, email, direccion, nombre, conn):
             conn.close()
 
 #Registrar un usuario en la base de datos
-def register_new_user(conn):
+def register_new_user():
     #Se pide el nombre de usuario, contraseña y rol del usuario
     username = input("Ingrese un nombre de usuario: ")
     password = input("Ingrese una contraseña: ")
@@ -153,6 +141,7 @@ def register_new_user(conn):
             return
 
         insert_query = "INSERT INTO usuarios (username, password, role) VALUES (%s, %s, %s)"
+        conn = connection()
         cursor = conn.cursor()
         cursor.execute(insert_query, (username, password, role))
         conn.commit()
@@ -163,11 +152,12 @@ def register_new_user(conn):
             nombre = input("Ingrese su nombre: ")
             email = input("Ingrese su email: ")
             direccion = input("Ingrese su dirección: ")
-            register_client(username, email, direccion, nombre, conn)
+            register_client(username, email, direccion, nombre)
     except psycopg2.Error as e:
         print("Error al registrar usuario:", e) 
 
-def registrarProducto():
+#Registrar un nuevo producto
+def registrar_producto():
     nombre = input("Ingrese el nombre del producto")
     descripcion = input("Ingrese la descripcion del producto")
     precio = input("Ingrese el precio del producto")
@@ -178,27 +168,36 @@ def registrarProducto():
             print("Ingrese valores correctos para el precio y el stock (tienen que ser numeros enteros)")
             return
         insert_query = "INSERT INTO productos (nombre, descripcion, precio, cantidad_stock) VALUES (%s, %s, %s, %s)"
-        cursor = connection().cursor()
+        conn = connection()
+        cursor = conn.cursor()
         cursor.execute(insert_query, (nombre, descripcion, precio, cantidad_stock))
         connection().commit()
         print("Producto registrado exitosamente.")
     except psycopg2.Error as e:
         print("Error al registrar prodructo:", e)
     finally:
-        if connection():
-            connection().close()
+        if conn:
+            conn.close()
 
-
+#Obtener el ultimo id asignado a una venta
 def obtener_ultimo_id():
-    cursor = connection().cursor()
+    conn = connection()
+    cursor = conn.cursor()
     cursor.execute("SELECT MAX(id) FROM ventas;")
     ultimo_id = cursor.fetchone()[0]
-    connection().close()
+    conn.close()
     return ultimo_id if ultimo_id is not None else 0
 
-
-def registrarVenta(producto, cantidad):
+#Administrador registra una nueva venta
+def registrar_venta():
+    producto = input("Ingrese el nombre del producto: ")
+    cant = input("Ingrese la cantidad a comprar: ")
     try:
+        if not cant.isdigit():
+            print("Ingrese una cantidad válida")
+            return
+        
+        cantidad = int(cant)
         conn = connection()
         cursor = conn.cursor()
         # Verificar si el producto existe y hay suficiente cantidad en stock
@@ -228,8 +227,136 @@ def registrarVenta(producto, cantidad):
         if conn:
             conn.close()
 
+#Ver informacion de un producto
+def ver_info_producto():
+    try:
+        conn = connection()
+        cursor = conn.cursor()
+        producto = input("Ingrese el nombre del producto: ")
+
+        # Consulta para obtener la información del producto
+        query = "SELECT nombre, descripcion, precio, cantidad_stock FROM productos WHERE nombre = %s"
+        cursor.execute(query, (producto,))
+        producto_info = cursor.fetchone()
+
+        if producto_info:
+            nombre, descripcion, precio, cantidad_stock = producto_info
+            print(f"Nombre: {nombre}")
+            print(f"Descripción: {descripcion}")
+            print(f"Precio: {precio}")
+            print(f"Cantidad en stock: {cantidad_stock}")
+        else:
+            print("Producto no encontrado.")
+
+    except psycopg2.Error as e:
+        print("Error al obtener la información del producto:", e)
+    finally:
+        if conn:
+            conn.close()
+
+#Obtener historial de ventas
+def obtener_registro_ventas():
+    try:
+        conn = connection()
+        cursor = conn.cursor()
+        # Consulta para obtener el registro de ventas con el monto total
+        query = """
+        SELECT v.id, v.producto, v.cantidad, p.precio, (v.cantidad * p.precio) AS monto_total
+        FROM ventas v
+        JOIN productos p ON v.producto = p.nombre;
+        """
+        cursor.execute(query)
+        ventas = cursor.fetchall()
+        
+        if ventas:
+            print("Registro de ventas:")
+            for venta in ventas:
+                id_venta, producto, cantidad, precio, monto_total = venta
+                print(f"ID Venta: {id_venta}, Producto: {producto}, Cantidad: {cantidad}, Precio: {precio}, Monto Total: {monto_total}")
+        else:
+            print("No hay ventas registradas.")
+        
+    except psycopg2.Error as e:
+        print("Error al obtener el registro de ventas:", e)
+    finally:
+        if conn:
+            conn.close()
+
+#Obtener los productos con un bajo stock
+def obtener_producto_bajo_stock():
+    try:
+        conn = connection()
+        cursor = conn.cursor()
+
+        # Consulta para obtener la información del producto
+        query = "SELECT nombre, descripcion, precio, cantidad_stock FROM productos WHERE cantidad_stock < 10"
+        cursor.execute(query)
+        productos = cursor.fetchall()
+        
+        
+        if productos:
+            for producto in productos: 
+                nombre, descripcion, precio, cantidad_stock = producto
+                print(f"Nombre: {nombre}")
+                print(f"Descripción: {descripcion}")
+                print(f"Precio: {precio}")
+                print(f"Cantidad en stock: {cantidad_stock}")
+        else:
+            print("Producto no encontrado.")
+
+    except psycopg2.Error as e:
+        print("Error al obtener la información del producto:", e)
+    finally:
+        if conn:
+            conn.close()
+
+#Actualizar el inventario
+def actualizar_inventario():
+    try:
+        conn = connection()
+        cursor = conn.cursor()
+
+        # Solicitar el nombre del producto a actualizar
+        producto = input("Ingrese el nombre del producto a actualizar: ")
+
+        # Verificar si el producto existe en la base de datos
+        select_query = "SELECT nombre, cantidad_stock, precio FROM productos WHERE nombre = %s"
+        cursor.execute(select_query, (producto,))
+        producto_info = cursor.fetchone()
+
+        if producto_info:
+            print(f"Producto encontrado: {producto_info[0]}")
+            print(f"Cantidad actual en stock: {producto_info[1]}")
+            print(f"Precio actual: {producto_info[2]}")
+
+            # Solicitar al administrador qué desea actualizar
+            opcion = input("¿Qué desea actualizar? (1: Cantidad en stock, 2: Precio): ")
+
+            if opcion == "1":
+                nueva_cantidad = int(input("Ingrese la nueva cantidad en stock: "))
+                update_query = "UPDATE productos SET cantidad_stock = %s WHERE nombre = %s"
+                cursor.execute(update_query, (nueva_cantidad, producto))
+                conn.commit()
+                print("Cantidad en stock actualizada exitosamente.")
+            elif opcion == "2":
+                nuevo_precio = float(input("Ingrese el nuevo precio: "))
+                update_query = "UPDATE productos SET precio = %s WHERE nombre = %s"
+                cursor.execute(update_query, (nuevo_precio, producto))
+                conn.commit()
+                print("Precio actualizado exitosamente.")
+            else:
+                print("Opción no válida.")
+        else:
+            print("Producto no encontrado.")
+
+    except psycopg2.Error as e:
+        print("Error al actualizar inventario:", e)
+    finally:
+        if conn:
+            conn.close()
+
 #Menu administrador
-def menuAdministrador(user):
+def menu_administrador(user):
     print("Menu administrador:")
     print("1. Registrar nuevo producto")
     print("2. Ver información de un producto")
@@ -242,28 +369,22 @@ def menuAdministrador(user):
 
     while(opcion != "7"):
         if opcion == "1":
-            registrarProducto()
+            registrar_producto()
 
         elif opcion == "2":
-            producto = input("Ingrese el nombre del producto: ")
-            query = "SELECT * FROM productos WHERE nombre = %s"
-            cursor = connection().cursor()
-            cursor.execute(query, (producto,))
-            print(cursor.fetchall())
-
+            ver_info_producto()
+            
         elif opcion == "3":
-            pass
+            actualizar_inventario()
 
         elif opcion == "4":
-            print(select_query("select * from productos where cantidad_stock < 10"))
+            obtener_producto_bajo_stock()
 
         elif opcion == "5":
-            producto = input("Ingrese el nombre del producto: ")
-            cantidad = int(input("Ingrese la cantidad a comprar: "))
-            registrarVenta(producto, cantidad)
+            registrar_venta()
 
         elif opcion == "6":
-            print(select_query("select * from ventas"))
+            obtener_registro_ventas()
 
         else:
             print("Ingrese una opcion valida")
@@ -284,20 +405,18 @@ def login():
             # Verificar el rol del usuario
             if user[2] == "administrador":
                 print("Bienvenido administrador.")
-                menuAdministrador(user)
+                menu_administrador(user)
             elif user[2] == "cliente":
                 print("Bienvenido cliente.")
                 # Coloca aquí el código para las acciones de un cliente
         else:
             print("Usuario no existe. Por favor, regístrese.")
-            register_new_user(conn)
+            register_new_user()
     except psycopg2.Error as e:
         print("Error al iniciar sesión:", e)
     finally:
         if conn:
             conn.close()
-
-
 
 if connection():
     login()
